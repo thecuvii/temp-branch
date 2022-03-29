af70cf44ee78c763af81f3e48bbe79cc36af6dda
Xwil <xw5427@gmail.com>
2022-03-29 19:13:06 +0800

    fix: merge错误分支

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,8 @@ const branches: Record<string, string> = {
 
 (async() => {
   const input = process.argv.slice(2)[0] as string
-  let targetBranch = branches[input]
+  const rawBranch = branches[input]
+  let targetBranch = rawBranch
 
   if (!targetBranch) {
     console.error('目标分支匹配失败，仅允许 \n', JSON.stringify(branches, null, 2))
@@ -24,7 +25,7 @@ const branches: Record<string, string> = {
     if (currentBranch) targetBranch = `temp-${currentBranch}-${targetBranch}-${today()}`
     if (targetBranch) {
       await execa('git', ['checkout', '-b', targetBranch])
-      await execa('git', ['merge', `origin/${targetBranch}`])
+      await execa('git', ['merge', `origin/${rawBranch}`])
     }
   }
   catch (e: unknown) {
